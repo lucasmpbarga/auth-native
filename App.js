a7c9b6b7f3523fd658818c5124bed68dfdb67b00
Lucas Barga <lucasmpbarga@gmail.com>
2018-12-14 02:17:00 -0200

    implementando login e logout, com autenticacao no firebase

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './src/commom/Commom';
+import { Header, Button, Spinner } from './src/commom/Commom';
 import firebase from 'firebase';
 import LoginForm from './src/LoginForm'
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount(){
         firebase.initializeApp({
             apiKey: "AIzaSyC8wzICtkmCLmvOcFFEHnozUAz7QZ-klh8",
@@ -14,13 +16,36 @@ class App extends Component {
             storageBucket: "auth-cc7b0.appspot.com",
             messagingSenderId: "732895620331"            
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user){
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent(){
+        switch (this.state.loggedIn) {
+            case true:
+                return(
+                    <Button onPress={() => firebase.auth().signOut()}> 
+                        Log Out
+                    </Button>
+                );  
+            case false:
+                return <LoginForm />
+            default:
+                return <Spinner size="large" />
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication"/>
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     };
